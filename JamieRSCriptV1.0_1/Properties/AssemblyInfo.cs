154a6744fab755d63616e153cc895a01cd6dfff3
JamieRenouf <136447798+JamieRenouf@users.noreply.github.com>
2023-06-13 14:20:38 +0100

    Add project files.

---
+++ JamieRSCriptV1.0_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JamieRSCriptV1.0_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("JamieRSCriptV1.0_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3BBD2C86-A662-4A3B-8BEF-DF6B6FEF3A26")]
+[assembly: AssemblyVersion("1.0.0.0")]
